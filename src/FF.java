cabff5c4ef6e3822cb1fb996f83907b322ca3fbe
yourUsername <yourEmail@domain.com>
2020-02-19 16:53:14 -0500

    hh

---
+++ src/FF.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FF {
+public static void main(String[]args) {
+
+    Scanner put = new Scanner( System.in );
+    int y;
+
+    int p;
+    //  multiplacation(y* p  );
+
+
+    System.out.println( "Enter integer1" );
+    y = put.nextInt();
+
+
+    System.out.println( "Enter integer2" );
+    p = put.nextInt();
+
+    System.out.println("Multiplaction: "+ y*p);
+
+
+}
+
+
+    }
+
+
